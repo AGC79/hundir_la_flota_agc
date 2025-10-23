61fca0714a551d7db99f1b84166d76ab4217f174
AGC79 <gudafreecam@gmail.com>
2025-10-23 22:21:59 +0200

    actualización de juego hlf

---
+++ code/main.py
@@ -0,0 +1,25 @@
+import funciones as f
+
+# Crear tableros y barcos
+tablero_jugador, barcos_jugador_total = f.crear_tablero_jugador()
+tablero_rival, barcos_rival_total = f.crear_tablero_rival()
+
+# Crear tableros vacíos para registrar los disparos
+tablero_jugador_disparos = f.tablero()
+tablero_rival_disparos = f.tablero()
+
+# Decidir quién empieza
+turno = f.inicio_partida()
+
+# Iniciar la partida
+f.jugar_partida(
+    turno,
+    tablero_jugador,
+    tablero_jugador_disparos,
+    barcos_jugador_total,
+    tablero_rival,
+    tablero_rival_disparos,
+    barcos_rival_total
+)
+
+
--- code/main_agc.py
+++
@@ -1,9 +0,0 @@
-import funciones_agc
-
-tablero_vacio = funciones_agc.tablero()
-
-tablero_jugador = funciones_agc.colocar_barcos(tablero_vacio)
-print("\n\nTablero generado para el jugador:\n\n", tablero_jugador, "\n\n")
-
-tablero_rival = funciones_agc.colocar_barcos(tablero_vacio)
-print("Tablero generado para el rival:\n\n", tablero_jugador, "\n\n")
